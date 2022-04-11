a4e84b9792e5148771ac91e34787609a227100db
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-04-11 14:14:57 +0300

    AccountController

--- Identity101/Models/Identity/ApplicationsUser.cs
+++ Identity101/Models/Identity/ApplicationsUser.cs
@@ -1,29 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
-namespace Identity101.Models.Identity
+namespace Identity101.Models.Identity;
+
+public class ApplicationUser : IdentityUser
 {
-    public class ApplicationsUser : IdentityUser
-    {
-        public string? Name { get; set; }
-        public string? Surname { get; set; }
-        public DateTime RegisterDate { get; set; } = DateTime.UtcNow;
-    }
-
-    public class ApplicationRole : IdentityRole
-    {
-        public string? Description { get; set; }
-
-        public ApplicationRole()
-        {
-
-        }
-
-        public ApplicationRole(string roleName, string description) : base(roleName)
-        {
-            this.Description = description;
-        }
-
-    }
-
+    [StringLength(50)]
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public DateTime RegisterDate { get; set; } = DateTime.UtcNow;
 
 }
