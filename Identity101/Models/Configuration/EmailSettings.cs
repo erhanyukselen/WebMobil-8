e71f959c44a2ecc6a9dfc39450af5a39dcc687bf
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-04-12 14:18:41 +0300

    Login okey

--- Identity101/Models/Configuration/EmailSettings.cs
+++ Identity101/Models/Configuration/EmailSettings.cs
@@ -5,6 +5,6 @@
         public string SenderMail { get; set; }
         public string Password { get; set; }
         public string Smtp { get; set; }
-        public int SmtpPort { get; internal set; }
+        public int SmtpPort { get; set; }
     }
 }
