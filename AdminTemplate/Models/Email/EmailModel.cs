92e7721397090f2fea2e968ebd09a0934c3494cb
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-04-19 15:03:15 +0300

    .

---
+++ AdminTemplate/Models/Email/EmailModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AdminTemplate.Models.Email
+{
+    public class EmailModel
+    {
+        public string Adress
+        {
+            get; set;
+        }
+        public string Name
+        {
+            get; set;
+        }
+    }
+}
---
+++ AdminTemplate/Models/Identity/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdminTemplate.Models.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(50)]
+        public string? Name
+        {
+            get; set;
+        }
+        public string? Surname
+        {
+            get; set;
+        }
+        public DateTime RegisterDate { get; set; } = DateTime.UtcNow;
+    }
+}
