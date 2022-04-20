b765cb9583d57b30b2d00e7d3bbab56c90d00b9d
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-04-20 10:23:05 +0300

    AccountController

---
+++ AdminTemplate/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminTemplate.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
