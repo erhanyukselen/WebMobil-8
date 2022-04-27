316152b289ea630c905d7d69c254ec06442a303c
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-04-27 15:03:07 +0300

    GeoLocation

--- FrontEnd/JavaScript/3.js
+++ FrontEnd/JavaScript/3.js
@@ -1,25 +1,50 @@
-console.log("3.js");
-
 var changeColor = () => {
     var rangeRed = document.getElementById("range-red");
     var rangeGreen = document.getElementById("range-green");
     var rangeBlue = document.getElementById("range-blue");
-
     var pickerDiv = document.getElementById("picker-div");
 
-    console.log([rangeRed.value, rangeGreen.value, rangeBlue.value]);
+    //console.log([rangeRed.value, rangeGreen.value, rangeBlue.value]);
     //var color = "rgb(" + rangeRed.value + "," + rangeGreen.value + "," + rangeBlue.value + ")";
-    var color = `rgb(${rangeRed.value},${rangeGreen.value},${rangeBlue.value})`; // üst satır ile aynı işlemi yapar backtik kullanılır stringleri birleştirmek için
-    var colorRev = `rgb(${255 - rangeRed.value},${255 - rangeGreen.value},${255 - rangeBlue.value})`; //255-renk dememizin sebebi yazının sürekli okunabilir olması için 
+    var color = `rgb(${rangeRed.value},${rangeGreen.value},${rangeBlue.value})`;
+    var colorRev = `rgb(${255 - rangeRed.value},${255 - rangeGreen.value},${255 - rangeBlue.value})`;
     pickerDiv.innerHTML = color;
     pickerDiv.style.backgroundColor = color;
-    pickerDiv.style.color = colorRev;  
+    pickerDiv.style.color = colorRev;
+}
+var copyClipboard = () => { //arrow function
+
+    Swal.fire({
+        icon: 'question',
+        title: 'Kopyalansın mı?',
+        showCancelButton: true,
+        confirmButtonText: 'Kopyala',
+        cancelButtonText: `İptal`,
+    }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        //console.log(result);
+        if (result.isConfirmed) {
+            var pickerDiv = document.getElementById("picker-div");
+            navigator.clipboard.writeText(pickerDiv.innerHTML);
+            Swal.fire('Kopyalandı!', pickerDiv.innerHTML, 'success')
+        } else if (result.isDenied) {
+            Swal.fire('İptal', 'Kopyalama işlemi iptal edildi', 'info')
+        }
+    })
 }
 
-    var copyClipboard = () => {
-        var pickerDiv = document.getElementById("picker-div");
-        navigator.clipboard.writeText(pickerDiv.innerHTML);
-        alert("Kopyalandı: "+pickerDiv.innerHTML);
-    }
+var divMouseOver = () => { 
+    var pickerDiv = document.getElementById("picker-div");
+    
+    pickerDiv.classList.add("animate__animated");
+    pickerDiv.classList.add("animate__pulse");
+}
+
+var divMouseLeave = () => { 
+    var pickerDiv = document.getElementById("picker-div");
+    
+    pickerDiv.classList.remove("animate__animated");
+    pickerDiv.classList.remove("animate__pulse");
+}
 
 changeColor();
