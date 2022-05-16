9b2b324a51b03fdad38498d276f4c4f37024f304
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-05-16 16:15:47 +0300

    Admintemplate projesindeki classlar uygun katmanlara taşındı

---
+++ WissenShop.Core/Configurations/EmailSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace WissenShop.Core.Configurations;
+
+public class EmailSettings
+{
+    public string SenderMail
+    {
+        get; set;
+    }
+    public string Password
+    {
+        get; set;
+    }
+    public string Smtp
+    {
+        get; set;
+    }
+    public int SmtpPort
+    {
+        get; set;
+    }
+}
